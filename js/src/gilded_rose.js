068c6e06a063ff8cd9ea2e18e94389ea84498199
Mark Salvin <marksalvin@users.noreply.github.com>
2020-03-15 12:37:00 +0000

    refactor: extract code to modify different types of items to functions
    - colocate conditional expressions for backstage passes

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,77 +6,72 @@ function Item(name, sell_in, quality) {
 
 var items = []
 
-function update_quality() {
+const updateAgedBrie = (item) => ({
+  ...item,
+  quality: item.quality + 1 <= 50 ? item.quality + 1 : 50,
+  sell_in: item.sell_in - 1,
+});
+
+const updateBackstagePass = (item) => {
+  if (item.sell_in > 10) {
+    return {
+      ...item,
+      quality: item.quality + 1 <= 50 ? item.quality + 1 : 50,
+      sell_in: item.sell_in - 1,
+    };
+  }
+
+  if (item.sell_in >= 6) {
+    return {
+      ...item,
+      quality: item.quality + 2 <= 50 ? item.quality + 2 : 50,
+      sell_in: item.sell_in - 1,
+    };
+  }
+
+  if (item.sell_in >= 1) {
+    return {
+      ...item,
+      quality: item.quality + 3 <= 50 ? item.quality + 3 : 50,
+      sell_in: item.sell_in - 1,
+    }
+  }
+
+  return {
+    ...item,
+    quality: 0,
+    sell_in: item.sell_in - 1,
+  };
+};
+
+const updateStandardItem = (item) => {
+  const updatedQuality = item.sell_in <= 0 ? item.quality - 2 : item.quality - 1;
+
+  return {
+    ...item,
+    quality: updatedQuality >= 0 ? updatedQuality : 0,
+    sell_in: item.sell_in - 1,
+  };
+};
+
+const update_quality = () => {
   if (!Array(items)) {
     return;
   }
 
   items = items.map(item => {
     if (item.name === 'Aged Brie') {
-      return {
-        ...item,
-        quality: item.quality + 1 <= 50 ? item.quality + 1 : 50,
-        sell_in: item.sell_in - 1,
-      };
+      return updateAgedBrie(item);
     }
 
     if (item.name === 'Sulfuras, Hand of Ragnaros') {
       return item;
     }
 
-    if (
-      item.name === 'Backstage passes to a TAFKAL80ETC concert'
-      && item.sell_in > 10
-    ) {
-      return {
-        ...item,
-        quality: item.quality + 1 <= 50 ? item.quality + 1 : 50,
-        sell_in: item.sell_in - 1,
-      };
+    if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+      return updateBackstagePass(item);
     }
 
-    if (
-      item.name === 'Backstage passes to a TAFKAL80ETC concert'
-      && item.sell_in <= 10
-      && item.sell_in >= 6
-    ) {
-      return {
-        ...item,
-        quality: item.quality + 2 <= 50 ? item.quality + 2 : 50,
-        sell_in: item.sell_in - 1,
-      };
-    }
-
-    if (
-      item.name === 'Backstage passes to a TAFKAL80ETC concert'
-      && item.sell_in <= 5
-      && item.sell_in >= 1
-    ) {
-      return {
-        ...item,
-        quality: item.quality + 3 <= 50 ? item.quality + 3 : 50,
-        sell_in: item.sell_in - 1,
-      };
-    }
-
-    if (
-      item.name === 'Backstage passes to a TAFKAL80ETC concert'
-      && item.sell_in <= 0
-    ) {
-      return {
-        ...item,
-        quality: 0,
-        sell_in: item.sell_in - 1,
-      };
-    }
-
-    // Default case
-    const updatedQuality = item.sell_in <= 0 ? item.quality - 2 : item.quality - 1;
-
-    return {
-      ...item,
-      quality: updatedQuality >= 0 ? updatedQuality : 0,
-      sell_in: item.sell_in - 1,
-    }
+    return updateStandardItem(item);
   });
 }
